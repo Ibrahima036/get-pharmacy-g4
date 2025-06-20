d57b5a237daa94bbf5a9755dc81c089b268c916d
ibrahima036 <hafismtechnologie@gmail.com>
2025-06-20 09:27:35 +0000

    add crud client

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Form\ClientForm;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('admin/client')]
+final class ClientController extends AbstractController
+{
+    #[Route(name: 'app_client_index', methods: ['GET'])]
+    public function index(ClientRepository $clientRepository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $pagination = $paginator->paginate(
+            $clientRepository->findBy([], ['id' => 'DESC']),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('client/index.html.twig', [
+            'clients' => $pagination,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_client_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $client = new Client();
+        $form = $this->createForm(ClientForm::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($client);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le client est ajouté.');
+
+            return $this->redirectToRoute('app_client_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('client/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_client_show', methods: ['GET'])]
+    public function show(Client $client): Response
+    {
+        return $this->render('client/show.html.twig', [
+            'client' => $client,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_client_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Client $client, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ClientForm::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Modification éffectué.');
+            return $this->redirectToRoute('app_client_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('client/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_client_delete', methods: ['POST'])]
+    public function delete(Request $request, Client $client, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $client->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($client);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_client_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ClientForm.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use App\Utils\ErrorMessage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class ClientForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => ErrorMessage::MESSAGE_DEFAULT]),
+                    new NotNull(['message' => ErrorMessage::MESSAGE_ERROR_VOID])
+                ],
+
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => "Nom",
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => ErrorMessage::MESSAGE_DEFAULT]),
+                    new NotNull(['message' => ErrorMessage::MESSAGE_ERROR_VOID])
+                ],
+
+            ])
+            ->add('assurance', TextType::class, [
+                'label' => "L'assurance",
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => ErrorMessage::MESSAGE_DEFAULT]),
+                    new NotNull(['message' => ErrorMessage::MESSAGE_ERROR_VOID])
+                ],
+
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
